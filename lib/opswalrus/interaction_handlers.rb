678ae53779236644d0f33c3fe386d20e7e0348ea
David Ellis <david@conquerthelawn.com>
2023-11-11 22:55:41 -0600

    gem 1.0.83

--- lib/opswalrus/interaction_handlers.rb
+++ lib/opswalrus/interaction_handlers.rb
@@ -60,16 +60,16 @@ module OpsWalrus
       end
       new_mapping.merge!(password_mappings) if password_mappings
 
-      debug(Style.green("mapping: #{mapping}"))
-      debug(Style.green("new_mapping: #{new_mapping}"))
-      debug(Style.green("new_mapping.empty?: #{new_mapping.empty?}"))
-      debug(Style.green("new_mapping == @input_mappings: #{new_mapping == @input_mappings}"))
+      # debug(Style.green("mapping: #{mapping}"))
+      # debug(Style.green("new_mapping: #{new_mapping}"))
+      # debug(Style.green("new_mapping.empty?: #{new_mapping.empty?}"))
+      # debug(Style.green("new_mapping == @input_mappings: #{new_mapping == @input_mappings}"))
       if new_mapping.empty? || new_mapping == @input_mappings
-        debug(Style.red("with_mapping -> reset"))
+        # debug(Style.red("with_mapping -> reset"))
         @online_matcher.reset
         yield self
       else
-        debug(Style.red("with_mapping -> new mapping"))
+        # debug(Style.red("with_mapping -> new mapping"))
         yield ScopedMappingInteractionHandler.new(new_mapping, lookback_window_chars)
       end
     end
@@ -97,7 +97,7 @@ module OpsWalrus
       # trace(Style.yellow("data=`#{data}`"))
       # trace(Style.yellow("buffer=#{@online_matcher.instance_exec { @buffer } }"))
       new_matches = @online_matcher.ingest(data)
-      debug(Style.yellow("new_matches=`#{new_matches}`"))
+      # debug(Style.yellow("new_matches=`#{new_matches}`"))
       response_data = new_matches.find_map do |online_match|
         mapped_output_value = @input_mappings[online_match.regex]
         case mapped_output_value
@@ -107,7 +107,7 @@ module OpsWalrus
           mapped_output_value
         end
       end
-      debug(Style.yellow("response_data=`#{response_data.inspect}`"))
+      # debug(Style.yellow("response_data=`#{response_data.inspect}`"))
 
       # response_data = @input_mappings.find_map do |pattern, mapped_output_value|
       #   pattern = pattern.is_a?(String) ? Regexp.new(Regexp.escape(pattern)) : pattern
